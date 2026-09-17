07b8988e92f7e221518cd5f0dc0c18d892866067
tiofour <adit.dit889@gmail.com>
2026-09-17 18:32:17 +0700

    Perbaiki jalur cache dan view container ke /tmp untuk vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
-// Jembatan untuk meneruskan permintaan dari Vercel ke file index Laravel
+
+// Paksa direktori penyimpanan dan tampilan mengarah ke /tmp yang bisa ditulis di Vercel
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/config.php';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp';
+
+// Meneruskan permintaan dari Vercel ke file index Laravel
 require __DIR__ . '/../public/index.php';
